50eb3ce6966efe0b4513cc3b066cf34f7670f67a
Tony Holdstock-Brown <tonyhb@gmail.com>
2011-08-12 15:50:04 +0100

    Adding base app files

---
+++ application/classes/app/core.php
@@ -0,0 +1,37 @@
+<?php defined('SYSPATH') or die('No direct script access.');
+
+/**
+ * Contains the low-level functionality of the app.
+ *
+ * This file manages initialisation and site loading.
+ *
+ * @packaged App
+ * @author Tony Holdstock-Brown
+ **/
+class App_Core
+{
+
+	const VERSION = "0.0.1";
+
+	/**
+	 * Stores the currently loaded site
+	 *
+	 * @var object
+	 **/
+	public static $site;
+
+	/**
+	 * Stores the currently loaded user
+	 *
+	 * @var object
+	 **/
+	public static $user;
+
+	/**
+	 * Stores the current language of the site
+	 *
+	 * @var object
+	 **/
+	public static $language;
+
+} // END class App_Core

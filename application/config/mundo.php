5c060f46b3c477dc206744b46e5dd67df4e0d3ff
Tony Holdstock-Brown <tonyhb@gmail.com>
2011-09-25 23:05:56 +0100

    Adding in replica sets for development

--- application/config/mundo.php
+++ application/config/mundo.php
@@ -14,17 +14,27 @@ switch(Kohana::$environment)
 	case Kohana::TESTING:
 		// Testing database
 		$enviromnent_settings = array(
-			'database'  => 'epithet-testing',
+			'servers' => 'mongodb://localhost:27017,localhost:27018,localhost:27019',
+			'database' => 'epithet-testing',
 		);
 		break;
 
 	case Kohana::DEVELOPMENT:
 		// Development database
 		$enviromnent_settings = array(
+			'servers' => 'mongodb://localhost:27017,localhost:27018,localhost:27019',
 			'database'  => 'epithet-dev',
 		);
 }
 
 return array(
-	'mongo_safe'   => TRUE,
+	'connect_options' => array(
+		'connect' => TRUE,
+		'replicaSet' => 'dev',
+	),
+	'query_options' => array(
+		'safe' => TRUE,
+		'fsync' => FALSE,
+		'timeout' => 10000,
+	),
 ) + $enviromnent_settings;

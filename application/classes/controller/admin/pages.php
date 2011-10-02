982e7daa16b34e4e86a3f8f9fe9dee4b808687b6
Tony Holdstock-Brown <tonyhb@gmail.com>
2011-10-02 19:26:53 +0100

    Adding initial class

---
+++ application/classes/controller/admin/pages.php
@@ -0,0 +1,16 @@
+<?php defined('SYSPATH') or die('No direct script access.');
+
+/**
+ * This class manages pages in the CMS
+ *
+ * @package App
+ **/
+class Controller_Admin_Pages extends Controller_Admin
+{
+
+	public function action_index()
+	{
+		// List all pages...
+	}
+
+} // END class Controller_Admin_Pages extends Controller_Admin

109336af200acf3f783bcf1c737b4dc3b6c3d4ab
Tony Holdstock-Brown <tonyhb@gmail.com>
2011-10-05 22:12:36 +0100

    Adding really simple base object class

--- application/classes/model/object.php
+++ application/classes/model/object.php
@@ -4,13 +4,49 @@
  * This is the base class for data in the app.
  *
  * An object is defined as a record of data in the CMS which isn't publicly
- * viewable from a URI. For example, a user would be an object, whereas a
- * product would be a resource (which extends this class).
+ * viewable from a URI. This is pretty much anything which isn't a page.
  *
- * @packaged App
+ * This allows people to create advanced models, such as job offers, and add
+ * metadata to objects without showing them as separate pages.
+ *
+ * All objects store revision histories of modifications on the 'data' field.
+ *
+ * @package App
  * @author Tony Holdstock-Brown
  **/
-class Model_Object
+class Model_Object extends Mundo_Object
 {
-	
+	protected $_collection = 'object';
+
+	/** 
+	 * Allow overrides in the data field only
+	 *
+	 * @var array
+	 */
+	protected $_schemaless = array(
+		'd',
+		'h',
+	);
+
+	protected $_fields = array(
+		'_id',
+		't', // Object type
+		'n', // Object name
+		'd', // Object data
+		'h', // Data history
+	);
+
+	protected $_rules = array(
+		't' => array(
+			array('not_empty'),
+			array('regex', array(':value', '#^[\w\s]+$#')),
+		),
+		'n' => array(
+			array('not_empty'),
+		),
+		'd' => array(
+			array('not_empty'),
+		),
+	);
+
 } // END class Model_Object

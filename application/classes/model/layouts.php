ea52330ee6721df9d0b9d5d704c01c66cd69471c
Tony Holdstock-Brown <tonyhb@gmail.com>
2011-12-20 17:11:51 +0000

    Adding CSS and Layout models

---
+++ application/classes/model/layouts.php
@@ -0,0 +1,10 @@
+<?php defined('SYSPATH') or die('No direct script access.');
+
+/**
+ * This is the model for layout objects in the CMS. Specifying this model allows us 
+ * to search for and show layout objects in the REST API.
+ *
+ * @package App
+ * @author Tony Holdstock-Brown
+ */
+class Model_Layouts extends Model_Objects {}

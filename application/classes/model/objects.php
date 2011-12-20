cdb1b38541cb6382cdf6deb1d89d0b487f00481c
Tony Holdstock-Brown <tonyhb@gmail.com>
2011-12-20 17:08:09 +0000

    Updating object model to allow default objects to be written via extending the object class. This allows default objects such as 'layouts', 'css' and 'snippets' to use the object class, its code and the same collection for sharding with no extra work

--- application/classes/model/objects.php
+++ application/classes/model/objects.php
@@ -6,10 +6,16 @@
  * An object is defined as a record of data in the CMS which isn't publicly
  * viewable from a URI. This is pretty much anything which isn't a page.
  *
- * This allows people to create advanced models, such as job offers, and add
- * metadata to objects without showing them as separate pages.
- *
  * All objects store revision histories of modifications on the 'data' field.
+ *
+ * This allows people to create advanced models, such as job offers, and add
+ * metadata to objects without showing them as separate pages. All default 
+ * objects, such as CSS and layouts, extend this.
+ *
+ * Default objects have a 'cstm' field value of FALSE, whilst custom objects 
+ * have a value of TRUE. This allows us to show only custom objects in an API 
+ * call to objects by setting this field in the call.
+ *
  *
  * @package App
  * @author Tony Holdstock-Brown
@@ -35,7 +41,7 @@ class Model_Objects extends App_Model
 
 	protected $_fields = array(
 		'_id',
-		'mod', // Last modified
+		'csm',  // Was this a custom object?
 		'type', // Object type
 		'name', // Object name
 		'data', // Object data
@@ -68,5 +74,73 @@ class Model_Objects extends App_Model
 		),
 		'binary' => array('hist')
 	);
+
+	protected $_hidden_fields = array(
+		'csm' => 0 // This is for internal use only.
+	);
+
+	/**
+	 * We extend the standard GET method to check whether we're accessing 
+	 * a default object (CSS, layouts, snippets etc.) or we're creating a new 
+	 * object.
+	 *
+	 * We could, realistically, make each default object its own model but we 
+	 * want the objects in the same mongo collection, which creates this problem 
+	 * for us.
+	 *
+	 */
+	public function API_Get($params = array())
+	{
+		if (get_class($this) != 'Model_Objects')
+		{
+			// This is a default object, for example CSS.
+			$this->set('csm', FALSE);
+
+			// Now we need to ensure we're searching for the correct default 
+			// object type.
+
+			$object_type = str_replace('Model_', '', get_class($this));
+			$object_type = strtolower($object_type);
+
+			if (array_key_exists('search', $params))
+			{
+				$position = strpos($params['search'], 'type:');
+
+				if ($position === FALSE)
+				{
+					$params['search'] .= ',type:'.$object_type;
+				}
+				else
+				{
+					// Get the position of the next item
+					$next_item_pos = strpos($params['search'], ',', $position);
+
+					$pre_type = substr($params['search'], 0, $position);
+
+					if ($next_item_pos !== FALSE)
+					{
+						$post_type = substr($params['search'], $next_item_pos);
+					}
+					else
+					{
+						$post_type = '';
+					}
+
+					$params['search'] = $pre_type.'type:'.$object_type.$post_type;
+				}
+			}
+			else
+			{
+				$params['search'] = 'type:'.$object_type;
+			}
+
+		}
+		else
+		{
+			$this->set('csm', TRUE);
+		}
+
+		return parent::API_Get($params);
+	}
 
 } // END class Model_Object

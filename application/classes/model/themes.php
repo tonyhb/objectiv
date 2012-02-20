51459c7dc7dcf363d4051c48a939c66aaef13894
Tony Holdstock-Brown <tonyhb@gmail.com>
2012-02-20 23:26:36 +0000

    Adding an initial theme model and NoSQL structure

---
+++ application/classes/model/themes.php
@@ -0,0 +1,79 @@
+<?php defined('SYSPATH') or die('No direct script access.');
+
+/**
+ *
+ * ## What is a theme?
+ *
+ *    A theme is a central collection of HTML templates, CSS files and 
+ *    JS/LESS/CSS files that are bundled together to create a theme package for 
+ *    a site on the CMS.
+ *
+ * ## Why bundle CSS, HTML and JS together?
+ *
+ *    Because this brings better heirachy to the models (there aren't CSS, JS 
+ *    and HTML models as a direct child of sites in the API)
+ *
+ *    This also allows us to create a default 'theme' package which users can 
+ *    learn on.
+ *
+ *    This will allow us to (eventually) package and possibly themes on a store.
+ *
+ * @package App
+ * @subpackage Theme
+ *
+ * @author Tony Holdstock-Brown
+ */
+class Model_Themes extends App_Model
+{
+	protected $_collection = 'theme';
+
+	protected $_fields = array(
+		'_id',
+		'name', // Theme name
+		'desc', // Description, optional.
+		'img',  // Thumbnail preview of theme
+		'lmod', // Last modification date
+
+		'obj.$.id',   // ID of the object in this theme
+		'obj.$.type', // Stores the type of object in the theme (HTML, CSS, JS, Less etc.)
+		'obj.$.name', // Denormalisation, storing the names of each object in this theme 
+
+		'site',
+		'acct'
+	);
+
+	protected $_rules = array(
+		'name' => array(
+			array('not_empty')
+		),
+		'lmod' => array(
+			array('Mundo::instance_of', array(':value', 'MongoDate')),
+		),
+		'site' => array(
+			array('Mundo::instance_of', array(':value', 'MongoId')),
+		),
+		'acct' => array(
+			array('Mundo::instance_of', array(':value', 'MongoId')),
+		),
+	);
+
+	protected $_metadata = array(
+		'binary' => array(
+			'img'
+		),
+		'children' => array(
+			'css',
+			'html',
+			'js',
+		),
+		'read_only' => array(
+			'lmod',
+		),
+	);
+
+	protected $_parent_coll = array(
+		'uri' => 'sites',
+		'mongo' => 'site'
+	);
+
+}

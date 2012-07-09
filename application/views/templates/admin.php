b9802a7df86efd2f6e0ffc109c96b084ec65f537
Tony Holdstock-Brown <tonyhb@gmail.com>
2012-07-09 16:26:06 +0100

    Adding SASS and new Login screen styles

--- application/views/templates/admin.php
+++ application/views/templates/admin.php
@@ -7,33 +7,28 @@
 <head>
 	<title><?php echo $title ?></title>
 	<meta charset="utf-8">
-
 	<?php foreach ($styles as $file => $type) echo HTML::style($file, array('media' => $type)), PHP_EOL, "\t" ?>
-
 	<?php foreach ($meta as $name => $content) echo '<meta name="'.$name.'" content="'.$content.'">', PHP_EOL, "\t" ?>
-
+	<link href='https://fonts.googleapis.com/css?family=Open+Sans:400italic,800italic,400,700,800' rel='stylesheet' type='text/css'>
 </head>
 <body>
-<header class='topbar'>
-	<nav class='fill'>
-		<div class='container'>
-			<a class='brand' href='/admin/'>Epithet</a>
-			<ul class='nav' style='height: 40px'>
-			<?php if (App::$user AND App_Auth::authorise_user(array('login', 'admin'))): ?>
-				<li><a href="<?php echo $base ?>/pages">Pages</a></li>
-					<?php if (App_Auth::authorise_user(array('view layouts', 'admin'))): ?>
-						<li><a href="<?php echo $base?>/layouts">Layouts</a></li>
-					<?php endif; ?>
-					<?php if (App_Auth::authorise_user(array('view css', 'admin'))): ?>
-						<li><a href="<?php echo $base?>/css">CSS</a></li>
-					<?php endif; ?>
-			<?php endif; ?>
-			</ul>
-		</div>
+<header>
+	<nav class='container'>
+		<ul class='nav'>
+		<?php if (App::$user AND App_Auth::authorise_user(array('login', 'admin'))): ?>
+			<li><a href="<?php echo $base ?>/pages">Pages</a></li>
+				<?php if (App_Auth::authorise_user(array('view layouts', 'admin'))): ?>
+					<li><a href="<?php echo $base?>/layouts">Layouts</a></li>
+				<?php endif; ?>
+				<?php if (App_Auth::authorise_user(array('view css', 'admin'))): ?>
+					<li><a href="<?php echo $base?>/css">CSS</a></li>
+				<?php endif; ?>
+		<?php endif; ?>
+		</ul>
 	</nav>
 </header>
 
-<div class='container' style='padding: 60px 0 0 0'>
+<div class='container'>
 	<?php echo $body ?>
 </div>
 
--- config.rb
+++ config.rb
@@ -6,7 +6,7 @@ require 'susy'
 http_path = "/"
 css_dir = "public_html/css"
 sass_dir = "sass"
-images_dir = "public_html/img"
+images_dir = "css/img"
 javascripts_dir = "public_html/js"
 
 # You can select your preferred output style here (can be overridden via the command line):
